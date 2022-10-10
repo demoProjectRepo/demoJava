88bae7e280b5a9f09e1acbe57156bb2a98efbf2a
demoProjectRepo <111334502+demoProjectRepo@users.noreply.github.com>
2022-10-10 20:06:28 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,3 +1,5 @@
-public class void main(String[] args){
-  System.out.println("Hey! We are here!");
+public class Hello{
+  public static void main(String[] args){
+     System.out.println("Hey! We are here!");
+  }
 }
